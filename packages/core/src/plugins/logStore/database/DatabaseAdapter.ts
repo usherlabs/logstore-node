f6e5efb9c92f60fcb22a41c87d7f090b943848c7
Raffael Campos <raffael.rcampos@gmail.com>
2024-01-05 09:30:27 -0300

    Refactor DatabaseAdapter and remove interface

--- packages/core/src/plugins/logStore/database/DatabaseAdapter.ts
+++ packages/core/src/plugins/logStore/database/DatabaseAdapter.ts
@@ -31,7 +31,6 @@ export type QueryDebugInfo =
 
 export abstract class DatabaseAdapter
 	extends DatabaseEventEmitter
-	implements DatabaseAdapterInterface
 {
 	constructor() {
 		super();
@@ -49,7 +48,7 @@ export abstract class DatabaseAdapter
 		limit?: number
 	): Readable;
 
-	abstract queryByMessageIds(messageIds: MessageID[]): Transform;
+	abstract queryByMessageIds(messageIds: MessageID[]): Readable;
 
 	abstract queryLast(
 		streamId: string,
@@ -79,70 +78,7 @@ export abstract class DatabaseAdapter
 
 	abstract store(streamMessage: StreamMessage): Promise<boolean>;
 
-	abstract getLastBuckets(
-		streamId: string,
-		partition: number,
-		limit?: number,
-		timestamp?: number
-	): Promise<Bucket[]>;
-
-	abstract upsertBucket(bucket: Bucket): Promise<{ bucket: Bucket; records: number }>;
-
 	abstract start(): Promise<void>;
 
 	abstract stop(): Promise<void>;
 }
-
-export type DatabaseAdapterInterface = {
-	queryRange(
-		streamId: string,
-		partition: number,
-		fromTimestamp: number,
-		fromSequenceNo: number,
-		toTimestamp: number,
-		toSequenceNo: number,
-		publisherId?: string,
-		msgChainId?: string,
-		limit?: number
-	): Readable;
-
-	queryByMessageIds(messageIds: MessageID[]): Transform;
-
-	queryLast(
-		streamId: string,
-		partition: number,
-		requestCount: number
-	): Readable;
-
-	getFirstMessageDateInStream(
-		streamId: string,
-		partition: number
-	): Promise<number | null>;
-
-	getLastMessageDateInStream(
-		streamId: string,
-		partition: number
-	): Promise<number | null>;
-
-	getNumberOfMessagesInStream(
-		streamId: string,
-		partition: number
-	): Promise<number>;
-
-	getTotalBytesInStream(streamId: string, partition: number): Promise<number>;
-
-	store(streamMessage: StreamMessage): Promise<boolean>;
-
-	getLastBuckets(
-		streamId: string,
-		partition: number,
-		limit?: number,
-		timestamp?: number
-	): Promise<Bucket[]>;
-
-	upsertBucket(bucket: Bucket): Promise<{ bucket: Bucket; records: number }>;
-
-	start(): Promise<void>;
-
-	stop(): Promise<void>;
-};
