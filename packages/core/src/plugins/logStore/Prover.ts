f42d913daf81cb2ce5284cf7db76be91e88e2e7e
xander <shuaibualexander@gmail.com>
2024-02-21 13:21:44 +0100

    refactor interface of prover class

--- packages/core/src/plugins/logStore/Prover.ts
+++ packages/core/src/plugins/logStore/Prover.ts
@@ -11,32 +11,34 @@ export type NetworkType = 'network' | 'standalone';
 export const proverSocketPath = `ipc:///tmp/test_sockets/test_pub`;
 
 export abstract class Prover {
+	logger: Logger;
 	subscription;
 	socketConnectionURL: string;
 	subscriber: zmq.Subscriber;
 	socketClient: SocketClient;
-	logger: Logger;
+	streamrClient: StreamrClient;
 
-	constructor(socketPath: string) {
+	constructor(socketPath: string, streamrClient: StreamrClient) {
 		const dealer = new zmq.Dealer();
 		this.subscriber = new zmq.Subscriber();
+
 		this.subscriber.connect(socketPath);
+		this.streamrClient = streamrClient;
 
 		const zmqTransportClient = new ZMQClientTransport(this.subscriber, dealer);
-
 		this.subscription = zmqTransportClient.start();
+
 		this.socketClient = new SocketClient(zmqTransportClient);
 		this.socketConnectionURL = socketPath;
 		this.logger = new Logger(module);
 	}
 
-	abstract handleNewProof(proof: TlsProof, streamrClient?: StreamrClient): void;
+	abstract handleNewProof(proof: TlsProof): void;
 
-	start(streamrClient?: StreamrClient) {
+	start() {
 		const proofSubscription = this.socketClient.subscribeToProofs({});
 		proofSubscription.responses.onNext(
-			(proof: undefined | TlsProof) =>
-				proof && this.handleNewProof(proof, streamrClient)
+			(proof: undefined | TlsProof) => proof && this.handleNewProof(proof)
 		);
 	}
 
--- packages/core/src/plugins/logStore/standalone/StandAloneProver.ts
+++ packages/core/src/plugins/logStore/standalone/StandAloneProver.ts
@@ -1,21 +1,8 @@
-import StreamrClient from 'streamr-client';
-
 import { BroadbandPublisher } from '../../../shared/BroadbandPublisher';
 import { TlsProof } from '../protobuff/generated/prover';
 import { Prover } from '../Prover';
 
 export class StandAloneProver extends Prover {
-	streamrClient: StreamrClient;
-
-	constructor(socketPath: string, streamrClient: StreamrClient) {
-		super(socketPath);
-		this.streamrClient = streamrClient;
-	}
-
-	async start() {
-		super.start(this.streamrClient);
-	}
-
 	async handleNewProof(proof: TlsProof) {
 		this.logger.info(`A new proof with id: ${proof.id} has been receieved`);
 		try {
