608c46e9c43b2b2a9608b7fde85661de686f7248
Victor Shevtsov <victorshevtsov@gmail.com>
2024-03-04 14:50:44 +0000

    Run the tests sequentially to avoid conflicts when port listening and calling smart contracts.

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
 	preset: 'ts-jest/presets/js-with-ts',
+	setupFiles: ['dotenv/config'],
 	testEnvironment: 'node',
 	clearMocks: true,
 	// can't use prettier 3 with jest
