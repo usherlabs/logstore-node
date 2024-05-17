9b43e9d71d283997124f327a2704a07fdf9e94ca
Victor Shevtsov <victorshevtsov@gmail.com>
2024-05-17 17:39:24 +0000

    Update PropagationList unit tests to be similar to AggregationList unit tests

--- packages/core/test/unit/plugins/logStore/PropagationList.test.ts
+++ packages/core/test/unit/plugins/logStore/PropagationList.test.ts
@@ -4,39 +4,70 @@ import { PropagationList } from '../../../../src/plugins/logStore/network/Propag
 import { mockStreamMessageRange } from './test-utils';
 
 type Action = {
-	node: 'primary' | 'foreign';
+	name: string;
+	source: 'primary' | 'foreign';
 } & (
 	| {
-			index: number;
-			data: MessageRef;
+			type: 'push';
+			item: MessageRef;
 	  }
 	| {
-			index: 'finalize';
+			type: 'finalize';
 	  }
 );
 
-interface Sequence {
+type Sequence = {
 	actions: Action[];
 	diff: MessageRef[];
 	isFinalized: boolean;
-}
+};
 
 // Test data
-const data = Array.from(mockStreamMessageRange(100200300, 100200303)).map((m) =>
-	m.getMessageRef()
-);
+const [D0, D1, D2] = Array.from(
+	mockStreamMessageRange(100200300, 100200303)
+).map((m) => m.getMessageRef());
 
-// Primary actions
-const P0: Action = { node: 'primary', index: 0, data: data[0] };
-const P1: Action = { node: 'primary', index: 1, data: data[1] };
-const P2: Action = { node: 'primary', index: 2, data: data[2] };
-const PF: Action = { node: 'primary', index: 'finalize' };
+// Primary actions (P - stands for Primary)
+const P0: Action = {
+	name: 'P0',
+	source: 'primary',
+	type: 'push',
+	item: D0,
+};
+const P1: Action = {
+	name: 'P1',
+	source: 'primary',
+	type: 'push',
+	item: D1,
+};
+const P2: Action = {
+	name: 'P2',
+	source: 'primary',
+	type: 'push',
+	item: D2,
+};
+const PF: Action = { name: 'PF', source: 'primary', type: 'finalize' };
 
-// Foreign actions
-const F0: Action = { node: 'foreign', index: 0, data: data[0] };
-const F1: Action = { node: 'foreign', index: 1, data: data[1] };
-const F2: Action = { node: 'foreign', index: 2, data: data[2] };
-const FF: Action = { node: 'foreign', index: 'finalize' };
+// Foreign actions (F - stands for Foreign)
+const F0: Action = {
+	name: 'F0',
+	source: 'foreign',
+	type: 'push',
+	item: D0,
+};
+const F1: Action = {
+	name: 'F1',
+	source: 'foreign',
+	type: 'push',
+	item: D1,
+};
+const F2: Action = {
+	name: 'F2',
+	source: 'foreign',
+	type: 'push',
+	item: D2,
+};
+const FF: Action = { name: 'FF', source: 'foreign', type: 'finalize' };
 
 function sequenceToString(sequence: Sequence) {
 	if (!sequence.actions.length) {
@@ -45,34 +76,25 @@ function sequenceToString(sequence: Sequence) {
 
 	return sequence.actions.reduce((name, action) => {
 		const delimiter = name ? '-' : '';
-		const nodeStr = action.node[0].toUpperCase();
-		const dataStr = action.index == 'finalize' ? 'F' : action.index;
-		return `${name}${delimiter}${nodeStr}${dataStr}`;
+		return `${name}${delimiter}${action.name}`;
 	}, '');
-}
-
-function tеstCasesFromSequences(sequences: Sequence[]) {
-	return sequences.map((sequence) => [
-		sequenceToString(sequence),
-		sequence,
-	]) as [string, Sequence][];
 }
 
 function runSequence(sequence: Sequence) {
 	const propagationList = new PropagationList();
 
 	for (const action of sequence.actions) {
-		switch (action.node) {
+		switch (action.source) {
 			case 'primary':
-				if (action.index !== 'finalize') {
-					propagationList.pushPrimary(data[action.index]);
+				if (action.type === 'push') {
+					propagationList.pushPrimary(action.item);
 				} else {
 					propagationList.finalizePrimary();
 				}
 				break;
 			case 'foreign':
-				if (action.index !== 'finalize') {
-					propagationList.pushForeign(data[action.index]);
+				if (action.type === 'push') {
+					propagationList.pushForeign(action.item);
 				} else {
 					propagationList.finalizeForeign();
 				}
@@ -86,23 +108,30 @@ function runSequence(sequence: Sequence) {
 	expect(propagationList.isFinalized).toEqual(sequence.isFinalized);
 }
 
+function testSequences(sequences: Sequence[]) {
+	const testCases = sequences.map((sequence) => [
+		sequenceToString(sequence),
+		sequence,
+	]) as [string, Sequence][];
+
+	test.each(testCases)('sequence of %s', (_name, sequence) => {
+		runSequence(sequence);
+	});
+}
+
 describe('PropagationList is in a proper state when', () => {
 	describe('there are no actions', () => {
-		const testCases = tеstCasesFromSequences([
+		testSequences([
 			{
 				actions: [],
 				diff: [],
 				isFinalized: false,
 			},
 		]);
-
-		test.each(testCases)('Sequence of %s', (_name, sequence) => {
-			runSequence(sequence);
-		});
 	});
 
 	describe('there are primary actions only', () => {
-		const testCases = tеstCasesFromSequences([
+		testSequences([
 			{
 				actions: [PF],
 				diff: [],
@@ -124,14 +153,10 @@ describe('PropagationList is in a proper state when', () => {
 				isFinalized: false,
 			},
 		]);
-
-		test.each(testCases)('Sequence of %s', (_name, sequence) => {
-			runSequence(sequence);
-		});
 	});
 
 	describe('there are foreign actions only', () => {
-		const testCases = tеstCasesFromSequences([
+		testSequences([
 			{
 				actions: [FF],
 				diff: [],
@@ -153,14 +178,10 @@ describe('PropagationList is in a proper state when', () => {
 				isFinalized: false,
 			},
 		]);
-
-		test.each(testCases)('Sequence of %s', (_name, sequence) => {
-			runSequence(sequence);
-		});
 	});
 
 	describe('there are the same primary and foreign actions', () => {
-		const testCases = tеstCasesFromSequences([
+		testSequences([
 			{
 				actions: [PF, FF],
 				diff: [],
@@ -182,99 +203,84 @@ describe('PropagationList is in a proper state when', () => {
 				isFinalized: true,
 			},
 		]);
-
-		test.each(testCases)('Sequence of %s', (_name, sequence) => {
-			runSequence(sequence);
-		});
 	});
 
 	describe('there are different primary and foreign actions', () => {
 		describe('and the primary actions run first', () => {
-			const testCases = tеstCasesFromSequences([
+			testSequences([
 				{
 					actions: [PF, F0],
-					diff: [F0.data],
+					diff: [D0],
 					isFinalized: false,
 				},
 				{
 					actions: [PF, F0, FF],
-					diff: [F0.data],
+					diff: [D0],
 					isFinalized: true,
 				},
 				{
 					actions: [PF, F0, F1],
-					diff: [F0.data, F1.data],
+					diff: [D0, D1],
 					isFinalized: false,
 				},
 				{
 					actions: [PF, F0, F1, FF],
-					diff: [F0.data, F1.data],
+					diff: [D0, D1],
 					isFinalized: true,
 				},
 			]);
-
-			test.each(testCases)('Sequence of %s', (_name, sequence) => {
-				runSequence(sequence);
-			});
 		});
 
 		describe('and the foreign actions run first', () => {
-			const testCases = tеstCasesFromSequences([
+			testSequences([
 				{
 					actions: [F0, PF],
-					diff: [F0.data],
+					diff: [D0],
 					isFinalized: false,
 				},
 				{
 					actions: [F0, FF, PF],
-					diff: [F0.data],
+					diff: [D0],
 					isFinalized: true,
 				},
 				{
 					actions: [F0, F1, PF],
-					diff: [F0.data, F1.data],
+					diff: [D0, D1],
 					isFinalized: false,
 				},
 				{
 					actions: [F0, F1, FF, PF],
-					diff: [F0.data, F1.data],
+					diff: [D0, D1],
 					isFinalized: true,
 				},
 			]);
-
-			test.each(testCases)('Sequence of %s', (_name, sequence) => {
-				runSequence(sequence);
-			});
 		});
 
 		describe('and the actions run in a mixed order', () => {
-			const testCases = tеstCasesFromSequences([
+			testSequences([
 				{
 					actions: [P1, PF, F2],
-					diff: [F2.data],
+					diff: [D2],
 					isFinalized: false,
 				},
 				{
 					actions: [F0, F1, P0, P1, F2, PF],
-					diff: [F2.data],
+					diff: [D2],
 					isFinalized: false,
 				},
 				{
 					actions: [P0, F0, F1, P2],
-					diff: [F1.data],
+					diff: [D1],
 					isFinalized: false,
 				},
 				{
 					actions: [P0, F0, F1, P2, FF, PF],
-					diff: [F1.data],
+					diff: [D1],
 					isFinalized: true,
 				},
 			]);
-
-			test.each(testCases)('Sequence of %s', (_name, sequence) => {
-				runSequence(sequence);
-			});
 		});
-		// TODO: Add more sophisticated test cases
 	});
+
+	// TODO: Add more sophisticated test cases
 });
